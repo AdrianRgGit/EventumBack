e23337346bacde7f2e65175d449a0b5213aac81b
saraoriola <saraoriola@outlook.com>
2023-09-25 11:39:25 +0200

    FUNCIONA OK OK OK OK .FINAL

--- routes/dashboards.js
+++ routes/dashboards.js
@@ -4,10 +4,6 @@ const DashboardController = require("../controllers/DashboardController");
 const { authentication, isAdmin } = require("../middleware/authentication");
 
 router.get("/event/:id", DashboardController.getEventById);
-router.get("/event/:id/getTimes", DashboardController.getTimes);
-router.get('/event/:id/getType', DashboardController.getType);
-router.get('/event/:id/getAttendees', DashboardController.getAttendees);
 
-router.get("/event/:id/users/getCountry", DashboardController.getCountry);
 
 module.exports = router;
